2617fb5454f3635677ccca7d383e547dea239caf
Rod Oliveira <jrodolfo@gmail.com>
2026-02-26 13:18:15 -0400

    add code generated by start.spring.io based on the dependencies: Maven, Java 25, Spring Boot 4, Spring Reactive Web (WebFlux), DevTools, Lombok, Docker Compose Support, SpringDoc OpenAPI, Validation, Actuator

---
+++ backend/src/test/java/net/jrodolfo/llmpetproject/backend/LlmPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jrodolfo.llmpetproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
